1c0b5b725daf3c5c1480e111ee1e3f316c0a98c4
Pierre Olivier <polivier@vt.edu>
2015-12-15 21:01:39 -0500

    Convert.py should be ok, it is now called by convert.sh btw

--- tecs/insert/traces/convert.py
+++ tecs/insert/traces/convert.py
@@ -2,6 +2,8 @@
 
 import sys, re, os
 
+CREATION_TIME_S=1000
+
 READ_RE="^(\d+.\d+);read;(\d+);(\d+);(\d+);(.+)$"
 READ_RE2="^(\d+);read;(\d+);(\d+);(\d+);(.+)$"
 WRITE_RE="^(\d+.\d+);write;(\d+);(\d+);(\d+);(.+)$"
@@ -11,110 +13,134 @@ OPEN_RE2="^(\d+);open;(\d+);(\d+);(\d+);(\d+);(.+)$"
 CLOSE_RE="^(\d+.\d+);close;(\d+);(.+)$"
 CLOSE_RE2="^(\d+);close;(\d+);(.+)$"
 
-if __name__ == "__main__":
+
+current_filenames = []
+last_addrs = {}
+first_time = -1
+
+def _warn_line(line):
+    prog_close2 = re.compile(CLOSE_RE2)
+    prog_open2 = re.compile(OPEN_RE2)
+    prog_write2 = re.compile(WRITE_RE2)
+    prog_read2 = re.compile(READ_RE2)
+    
+    if prog_read2.match(line):
+	print "WARNING READ 2 MATCH"
+	return -1
+    if prog_write2.match(line):
+	print "WARNING WRITE 2 MATCH"
+	return -2
+    if prog_open2.match(line):
+	print "WARNING OPEN 2 MATCH"
+	return -3
+    if prog_close2.match(line):
+	print "WARNING CLOSE 2 MATCH"
+	return -4
+    return 0
+    
+def _common_ops(time, name):
+    global current_filenames
+    global last_addrs
+    global first_time
+    
+    if first_time == -1:
+	first_time = time
+	
+    if name not in current_filenames:
+	current_filenames.append(name)
+	last_addrs[name] = 0
+	
+def _common_rw_ops(name, offset, size):
+    global last_addrs
+    if offset + size > last_addrs[name]:
+	last_addrs[name] = offset + size
+		
+def _conv_time(time):
+    global first_time
+    if first_time == -1:
+	print "ERROR Time"
+	sys.exit(-1)
+	
+    return time - first_time + CREATION_TIME_S
+    
+if __name__ == "__main__":    
+    res = ""
+    
     if len(sys.argv) != 2:
 	print "Usage : " + sys.argv[0] + " <trace_file>"
 	
     prog_read = re.compile(READ_RE)
-    prog_read2 = re.compile(READ_RE2)
     prog_write = re.compile(WRITE_RE)
-    prog_write2 = re.compile(WRITE_RE2)
     prog_open = re.compile(OPEN_RE)
-    prog_open2 = re.compile(OPEN_RE2)
     prog_close = re.compile(CLOSE_RE)
-    prog_close2 = re.compile(CLOSE_RE2)
 
     with open(sys.argv[1], "r") as f:
 	lines = f.readlines()
-	
-    current_filenames = []
-    last_addrs = {}
-	
+    
     for line in lines:
 	if prog_read.match(line):
 	    groups = prog_read.search(line).groups()
-	    time = groups[0]
-	    fd = groups[1]
-	    offset = groups[2]
-	    size = groups[3]
+	    time = float(groups[0])
+	    fd = int(groups[1])
+	    offset = int(groups[2])
+	    size = int(groups[3])
 	    name = groups[4]
 	    
-	    if name not in current_filenames:
-		print "new: " + name
-		current_filenames.append(name)
-		last_addrs[name] = 0
-		
-	    if offset + size > last_addrs[name]:
-		last_addrs[name] = offset + size
+	    _common_ops(time, name)
+	    _common_rw_ops(name, offset, size)
 		
 	    inode_num = current_filenames.index(name)
 	    
-	    print time + ";read;" + str(inode_num) + ";" + str(offset) + ";" + \
-		str(size)
-	    
-	    #~ print line.replace(name, str(inode_num))
+	    res += str(_conv_time(time)) + ";read;" + str(inode_num) + ";" + str(offset) + ";" + \
+		str(size) + "\n"
 	    
 	# WRITE
 	if prog_write.match(line):
 	    groups = prog_write.search(line).groups()
-	    time = groups[0]
-	    fd = groups[1]
-	    offset = groups[2]
-	    size = groups[3]
+	    time = float(groups[0])
+	    fd = int(groups[1])
+	    offset = int(groups[2])
+	    size = int(groups[3])
 	    name = groups[4]
 	    
-	    if name not in current_filenames:
-		print "new: " + name
-		current_filenames.append(name)
-		last_addrs[name] = 0
-		
-	    if offset + size > last_addrs[name]:
-		last_addrs[name] = offset + size
+	    _common_ops(time, name)
+	    _common_rw_ops(name, offset, size)
 		
 	    inode_num = current_filenames.index(name)
 	    
-	    print time + ";write;" + str(inode_num) + ";" + str(offset) + ";" + \
-		str(size)
+	    res += str(_conv_time(time)) + ";write;" + str(inode_num) + ";" + str(offset) + ";" + \
+		str(size) + "\n"
 	    
 	if prog_open.match(line):
 	    groups = prog_open.search(line).groups()
-	    time = groups[0]
-	    name = groups[4]
+	    time = float(groups[0])
+	    name = groups[5]
 	    
-	    if name not in current_filenames:
-		print "new: " + name
-		current_filenames.append(name)
-		last_addrs[name] = 0
+	    _common_ops(time, name)
 		
 	    inode_num = current_filenames.index(name)
 	    
-	    print time + ";open;" + str(inode_num) + ";0;0;0"
+	    res += str(_conv_time(time)) + ";open;" + str(inode_num) + ";0;0;0" + "\n"
 	    
 	if prog_close.match(line):
 	    groups = prog_close.search(line).groups()
-	    time = groups[0]
+	    time = float(groups[0])
 	    name = groups[2]
 	    
-	    if name not in current_filenames:
-		print "new: " + name
-		current_filenames.append(name)
-		last_addrs[name] = 0
+	    _common_ops(time, name)
 		
 	    inode_num = current_filenames.index(name)
 	    
-	    print time + ";close;" + str(inode_num)
+	    res += str(_conv_time(time)) + ";close;" + str(inode_num) + "\n"
 	    
-	if prog_read2.match(line):
-	    print "WARNING READ 2 MATCH"
-	    os.exit()
-	if prog_write2.match(line):
-	    print "WARNING WRITE 2 MATCH"
-	    os.exit()
-	if prog_open2.match(line):
-	    print "WARNING OPEN 2 MATCH"
-	    os.exit()
-	if prog_close2.match(line):
-	    print "WARNING CLOSE 2 MATCH"
-	    os.exit()
+	if _warn_line(line) != 0:
+	    sys.exit(-1)
+    
+    i=0
+    for f in last_addrs:
+	print str(i) + ".0;open;" + str(current_filenames.index(f)) + ";0;0;0"
+	print str(i) + ".1;write;" + str(current_filenames.index(f)) + ";0;" + str(last_addrs[f])
+	print str(i) + ".1;close;" + str(current_filenames.index(f))
+	i += 1
 	
-    # TODO process last addr
+    print res
--- tecs/insert/traces/convert.sh
+++ tecs/insert/traces/convert.sh
@@ -4,6 +4,6 @@ for trace in `ls hamza_format/*`; do
     filename=$(basename "$trace")
     output="${filename%.*}"
     
-    sed -e "s/\/mnt\/flash\/test.db$/0/" -e "s/\/mnt\/flash\/test.db-journal$/1/" -e "s/\/mnt\/flash$/2/" $trace > $output.conv.csv
+    ./convert.py  $trace > $output.conv.csv
     
 done
